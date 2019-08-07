86380ae504edc26fdd6fb4e7854f84e2f381432f
Edwin Coronado <edcoronado.17@gmail.com>
2019-08-06 21:29:23 -0700

    Added under construction message.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Edwin Coronado
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </h1>
+        <h3>Website Under Construction</h3>
+        <h6>Visit my <a className="App-link" href="https://edwincoronado.com">photography site</a> or my <a className="App-link" href="https://www.linkedin.com/in/edwincoronado/">LinkedIn profile</a> in the meantime 😊</h6>
+
       </header>
     </div>
   );
