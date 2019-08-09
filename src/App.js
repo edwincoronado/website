361357aa65afd48f3ead112d314735b285545fdd
Edwin Coronado <edcoronado.17@gmail.com>
2019-08-08 22:28:51 -0700

    Fixed some minor padding issue.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => {
         className="bg-black object-center h-screen px-20 md:px-32 xl:px-32"
         src={title}
       />
-      <div className="bg-desert-sand w-max h-56">
+      <div className="bg-desert-sand w-max h-auto">
         <h1 className="font-sans text-2xl sm:text-3xl lg:text-6xl text-center py-12">
           Website Under Construction
         </h1>
-        <h6 className="font-sans px-6 text-m word-break lg:text-3xl text-center">
+        <h6 className="font-sans px-6 text-m word-break lg:text-3xl text-center pb-12">
           Visit my{" "}
           <a
             className="text-blue-700 font-medium"
