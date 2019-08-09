368ab4cddb8d9cdc388677f6926a97c8c048d089
Edwin Coronado <edcoronado.17@gmail.com>
2019-08-08 22:24:39 -0700

    Custom "Under Construction" page.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,28 @@ const App = () => {
         className="bg-black object-center h-screen px-20 md:px-32 xl:px-32"
         src={title}
       />
-      <div className="bg-desert-sand w-max h-32"></div>
+      <div className="bg-desert-sand w-max h-56">
+        <h1 className="font-sans text-2xl sm:text-3xl lg:text-6xl text-center py-12">
+          Website Under Construction
+        </h1>
+        <h6 className="font-sans px-6 text-m word-break lg:text-3xl text-center">
+          Visit my{" "}
+          <a
+            className="text-blue-700 font-medium"
+            href="https://edwincoronado.com"
+          >
+            photography site
+          </a>{" "}
+          or my{" "}
+          <a
+            className="text-blue-700 font-medium"
+            href="https://www.linkedin.com/in/edwincoronado/"
+          >
+            LinkedIn profile
+          </a>{" "}
+          in the meantime 😊
+        </h6>
+      </div>
     </>
   );
 };
