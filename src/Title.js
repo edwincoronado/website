fd87a78ba1441531fb7d2753404f9a26bcd83841
Edwin Coronado <edcoronado.17@gmail.com>
2019-08-22 19:19:48 -0700

    First version of new design. Need to add favicon and meta for SEO.

--- src/Title.js
+++ src/Title.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Title = () => {
-  const baseCss =
-    "text-white leading-none font-bold tracking-wider absolute bottom-0 left-0 uppercase";
+  const h1Css =
+    "text-white leading-none font-bolder tracking-widest absolute bottom-0 left-0 uppercase";
   return (
-    <section className="bg-fixed bg-black h-screen w-screen p-20" id="home">
-      <h1 className={`text-4xl pb-20 pl-12 sm:text-5xl sm:pl-16 md:text-6xl md:pl-20 lg:text-8xl lg:pl-20 xl:text-9xl xl:pl-24 text-white ${baseCss}`}>
+    <section className="bg-fixed bg-black h-screen w-screen" id="home">
+      <h1 className={`text-4.5xl mb-12 pl-12 sm:text-5xl sm:pl-16 md:text-6xl md:pl-20 lg:text-8xl lg:pl-20 xl:text-9xl xl:pl-24 text-white ${h1Css}`}>
         Edwin
         <br />
         <span className="text-desert-sand">Coronado</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "desert-sand": "#D0B080"
+        "desert-sand": "#D0B080",
+        "almost-black-200": "#262629",
+        "almost-black-100": "#313136",
+        "orchid-plum": "#9080d0"
       },
       fontSize: {
+        "4.5xl": "2.65rem",
         "7xl": "5rem",
         "8xl": "6rem",
         "9xl": "7rem"
+      },
+      fontWeight: {
+        "bolder": 750
+      },
+      fontFamily: {
+        "Rubik": ['Rubik', 'sans-serif']
       }
     }
   },
