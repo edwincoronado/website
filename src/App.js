55625578694ce7676e41e20c1e8f8a54e048a763
Edwin Coronado <edcoronado.17@gmail.com>
2019-09-08 16:57:17 -0700

    Adding scroll reveal and font.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Title from "./Title";
 import About from "./About";
+import ScrollReveal from 'scrollreveal'
 
 const App = () => {
+  React.useEffect(() => {
+    const options = {
+      delay: 200,
+      reset: true
+    };
+    ScrollReveal().reveal('h1', options);
+    ScrollReveal().reveal('h2', options);
+    ScrollReveal().reveal('p', options);
+  });
   return (
     <>
       <Title />
